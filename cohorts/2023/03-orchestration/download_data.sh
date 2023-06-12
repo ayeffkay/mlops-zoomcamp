d3d1039fb07bd7bb8dbcc3ee002d45f33f6fbdb2
ayeffkay <chatton1@mail.ru>
2023-06-12 15:20:49 +0300

    feat: add prefect files for deployment

---
+++ cohorts/2023/03-orchestration/download_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p data && cd data
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-01.parquet
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2023-02.parquet
